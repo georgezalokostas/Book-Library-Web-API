04fdd8fed9f8ce9c21dbedda16bfe40df847bb4b
George Zalokostas <gzalos6@gmail.com>
2022-11-13 15:44:40 +0200

    Added Books Migration

--- BookLibrary_API/Models/Book.cs
+++ BookLibrary_API/Models/Book.cs
@@ -8,6 +8,6 @@ public class Book
     public int Id { get; set; }
     public string Title { get; set; }
     public string Author { get; set; }
-    public DateOnly ReleaseDate { get; set; }
+    public DateTime ReleaseDate { get; set; }
     public bool IsAvailable { get; set; }
 }
